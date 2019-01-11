67ff7a0db4348344005c508392e9e9c3ce5e993f
Tyler Nicholson <tylernicholson@Tylers-MacBook-Pro.local>
2019-01-11 16:02:42 -0700

    completed class

--- Car.js
+++ Car.js
@@ -1,5 +1,15 @@
 class Car{
-    
+    constructor(color,convertible){
+        this.color = color
+        this.convertible = convertible
+        this.speed = 0
+    }
+    accelerate(spd) {
+        this.speed += spd
+    }
+    decelerate(spd){
+        this.speed -= spd
+    }
 }
 
 module.exports = Car;
