70e8ad01c8004830d26dd6a70213c5d0e97da20b
Ryan Fleharty <ryan.fleharty@gmail.com>
2019-01-11 14:47:47 -0700

    changed carjs

--- Car.js
+++ Car.js
@@ -1,9 +1,5 @@
 class Car{
-    constructor(color, convertible){
-        this.color = color;
-        this.convertible = convertible;
-        this.speed = 0;
-    }
+    
 }
 
 module.exports = Car;
